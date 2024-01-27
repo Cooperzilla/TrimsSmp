b071714a3023c22494d424dc5a05a0be3e1d65d9
Cooperzilla <cooperye09@gmail.com>
2024-01-27 16:38:55 -0600

    added a utility for configuring trims

---
+++ src/main/java/me/cooperzilla/trimssmp/utils/NumUtils.java
@@ -0,0 +1,19 @@
+package me.cooperzilla.trimssmp.utils;
+
+public class NumUtils {
+    public static int getNum(int num) {
+        if (num-1 > 0) {
+            return num + ((num-1) * 9);
+        } else {
+            return 1;
+        }
+    }
+
+    public static int seconds(int num) {
+        return num * 20;
+    }
+
+    public static int minutes(int num) {
+        return num * 60 * 20;
+    }
+}
