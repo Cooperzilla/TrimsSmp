4ae74fa4d3b11c45d03cf480396dcd3f26eeb657
Cooperzilla <cooperye09@gmail.com>
2024-01-27 13:01:54 -0600

    registered rib

--- src/main/java/me/cooperzilla/trimssmp/TrimsSmp.java
+++ src/main/java/me/cooperzilla/trimssmp/TrimsSmp.java
@@ -1,10 +1,7 @@
 package me.cooperzilla.trimssmp;
 
 import me.cooperzilla.trimssmp.crafting.Smithing;
-import me.cooperzilla.trimssmp.trims.Coast;
-import me.cooperzilla.trimssmp.trims.Raiser;
-import me.cooperzilla.trimssmp.trims.Ward;
-import me.cooperzilla.trimssmp.trims.Wayfinder;
+import me.cooperzilla.trimssmp.trims.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,6 +14,7 @@ public class TrimsSmp extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Wayfinder(), this);
         getServer().getPluginManager().registerEvents(new Ward(), this);
         getServer().getPluginManager().registerEvents(new Raiser(), this);
+        getServer().getPluginManager().registerEvents(new Rib(), this);
 
         getLogger().info("Listeners Registered!!!");
 
