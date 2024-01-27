1b69c921dea89ea29de6f2f7b8f18a86716bfb1b
Cooperzilla <cooperye09@gmail.com>
2024-01-27 00:40:34 -0600

    added broken crafting and updated stuff so it can compile

---
+++ src/main/java/me/cooperzilla/trimssmp/Smithing.java
@@ -0,0 +1,21 @@
+package me.cooperzilla.trimssmp;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Tag;
+import org.bukkit.inventory.*;
+
+public class Smithing {
+    static Recipe main() {
+        NamespacedKey ns = new NamespacedKey(new TrimsSmp(), "sword_smith");
+
+        SmithingRecipe recipe = new SmithingTransformRecipe(
+                ns,
+                new ItemStack(Material.DIAMOND_SWORD),
+                new RecipeChoice.MaterialChoice(Tag.ITEMS_TRIM_TEMPLATES),
+                new RecipeChoice.MaterialChoice(Tag.ITEMS_SWORDS),
+                new RecipeChoice.MaterialChoice(Tag.ITEMS_TRIM_MATERIALS));
+
+        return recipe;
+    }
+}
--- src/main/java/me/cooperzilla/trimssmp/TrimsSmp.java
+++ src/main/java/me/cooperzilla/trimssmp/TrimsSmp.java
@@ -1,15 +1,22 @@
 package me.cooperzilla.trimssmp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TrimsSmp extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("plugin loaded !!!");
+        getLogger().info("plugin loaded!!!");
+
         getServer().getPluginManager().registerEvents(new CoastTrim(), this);
         getServer().getPluginManager().registerEvents(new Wayfinder(), this);
         getServer().getPluginManager().registerEvents(new Ward(), this);
         getServer().getPluginManager().registerEvents(new Raiser(), this);
 
+        getLogger().info("Listeners Registered!!!");
+
+        Bukkit.addRecipe(Smithing.main());
+
+        getLogger().info("Recipes Registered!!!");
     }
 }
