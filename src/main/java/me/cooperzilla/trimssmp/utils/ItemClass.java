497ff7d704f1434e6724279d85190c62aa60917f
Cooperzilla <cooperye09@gmail.com>
2024-01-27 20:56:35 -0600

    made class for items to extend

---
+++ src/main/java/me/cooperzilla/trimssmp/utils/ItemClass.java
@@ -0,0 +1,58 @@
+package me.cooperzilla.trimssmp.utils;
+
+import org.bukkit.Color;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class ItemClass implements Listener {
+
+    private final Integer num;
+    private final Long COOLDOWN_DURATION;
+    private final String str;
+
+    public ItemClass(Integer num, Long cooldownDuration, String str) {
+        this.num = num;
+        this.COOLDOWN_DURATION = cooldownDuration;
+        this.str = str;
+    }
+
+    @EventHandler
+    public void onCraftItem(CraftItemEvent event) {
+        ItemStack result = event.getRecipe().getResult();
+        if (CheaksUtils.hasCorrectIngredients(event, num)) {
+            Color color = ColorUtils.getColorFromAdjacentOre(event);
+            if (color != null) {
+                ColorUtils.applyColor(result, color, num);
+            }
+        } else {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = event.getItem();
+
+        if (item == null) {
+            return;
+        }
+
+        if (CheaksUtils.hasTrim(item, num)) {
+            if (event.getAction().name().contains("RIGHT_CLICK")) {
+                if (!(player.hasMetadata(str))) {
+                    run(event);
+                    CooldownUtils.setCooldown(player, str, COOLDOWN_DURATION);
+                } else {
+                    player.sendMessage("Ability on cooldown!");
+                }
+            }
+        }
+    }
+
+    protected abstract void run(PlayerInteractEvent event);
+}
